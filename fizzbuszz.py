eaad9bfa08d67937fe3c7651c5d7395ee6def412
ptg0811 <ptg0811@gmail.com>
2023-05-02 11:37:01 +0900

    refactor: change j to k

--- fizzbuszz.py
+++ fizzbuszz.py
@@ -1,9 +1,9 @@
-for j in range(1, 16+1):
-    if j%15==0:
+for k in range(1, 16+1):
+    if k%15==0:
         print('fizzbuzz')
-    elif j%3==0:
+    elif k%3==0:
         print('fizz')
-    elif j%5==0:
+    elif k%5==0:
         print('buzz')
     else:
-        print(f'{j}')
+        print(f'{k}')
