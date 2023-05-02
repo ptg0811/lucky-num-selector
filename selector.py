fdfc63f7729f059b7b9a661234394898c540510f
ptg0811 <ptg0811@gmail.com>
2023-05-02 11:08:56 +0900

    refactor: functionalize sampling process

--- selector.py
+++ selector.py
@@ -1,5 +1,14 @@
-import random
+import numpy as np
 
-result = random.sample(range(1,45+1), k=6)
+# 사용자가 원하는 횟수를 입력 받습니다.
+n = int(input("몇 번 숫자를 뽑으시겠습니까? "))
 
-print(result)
+# 1부터 45까지의 숫자를 리스트로 만듭니다.
+numbers = list(range(1, 46))
+
+# n번 반복하면서 무작위로 6개의 숫자를 뽑습니다.
+for i in range(n):
+    # numpy 모듈을 사용하여 무작위로 6개의 숫자를 선택합니다.
+    selected_numbers = np.random.choice(numbers, size=6, replace=False)
+    # 선택된 숫자를 출력합니다.
+    print(selected_numbers)
